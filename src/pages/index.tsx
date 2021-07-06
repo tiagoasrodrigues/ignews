36393bc73eae23c5f18b2dabc1dfe06ac11467de
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-07-06 17:10:52 -0300

    :wrench: chore: add Static Site Generation in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
 
@@ -6,6 +6,15 @@ import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Post do Blog 
+
+// Conteudo (SSG)
+// Comentários (Client-side)
 
 interface HomeProps {
   product: {
@@ -38,7 +47,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JAJKGAZwRgethYPbm651luU')
 
   const product = {
@@ -52,6 +61,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
